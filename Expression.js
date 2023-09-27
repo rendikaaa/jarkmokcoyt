8f7ede3284957ded9711fceb744f3cfea5361875
rendikaaa <114813064+rendikaaa@users.noreply.github.com>
2023-09-26 21:51:47 -0700

    Add files via upload

---
+++ Expression.js
@@ -0,0 +1,6 @@
+const name = "Rendika Febrian"
+const value = 80;
+
+const template = `name : ${name}, lulus : ${value > 75}`;
+
+console.info(template)
---
+++ HelloWorld.js
@@ -0,0 +1,18 @@
+const Nama = "Rendika Febrian"
+const NIM = 20220040033
+
+// aritmatika Pada JavaScript
+
+const Angka1 = 20
+const Angka2 = 10
+
+const Tambah = Angka1 + Angka2 ;
+const Kurang = Angka1 - Angka2 ;
+const Kali = Angka1 * Angka2 ;
+const Bagi = Angka1 / Angka2 ;
+
+console.log('Nama Saya' + Nama )
+console.log('NIM Saya' + NIM )
+console.log("Pertambahan " + Tambah )
+console.log("Perkalian " + Kali)
+console.log("Pembagian "+ Bagi )
